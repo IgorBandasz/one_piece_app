7762ebbc88f332535c63235fd9676ab810e34652
Igor Bandasz <profigorbandasz@gmail.com>
2024-03-14 22:26:07 -0300

    moldura

--- src/layouts/Tela_inicial.tsx
+++ src/layouts/Tela_inicial.tsx
@@ -1,19 +1,46 @@
-import { Text, StyleSheet, View, Pressable, Image } from "react-native";
+import { Text, StyleSheet, View, Pressable, Image, ImageBackground } from "react-native";
 import { Tela_inicialprops } from "../navigator/navigator_types";
+import React, { Children, ReactNode } from "react";
+
+type MolduraProps = {
+    children: ReactNode
+}
+
+const Moldura: React.FC<MolduraProps> = ({ children }) => {
+    return (
+        <ImageBackground
+            source={require("../images/moldurahdpi.png")} style={styles.moldura}>
+            <View style={styles.preenchimento_moldura}>
+                {children}
+            </View>
+        </ImageBackground>                   
+    )
+
+}
 
 export default ({ navigation }: Tela_inicialprops) => {
     return (
         <View style={styles.container}>
-            <View style={styles.center}>
-
-                <Pressable style={styles.botao}
-                    //onPress={() => navigation.navigate('Quant_clicks')}
-                    >
-
-                    <Text style={{ fontSize: 20 }}>Botão</Text>
-                </Pressable>
-                <Image source={require("../images/logo.png")}/>
-            </View>
+            <ImageBackground
+                source={require("../images/fundo_agua.jpg")} style={styles.background}>
+                <View style={styles.header}>
+                    <Image source={require("../images/logo.png")} style={styles.logo}/>
+                </View>    
+                <View style={styles.body}>
+                    <Pressable style={styles.botao}
+                        //onPress={() => navigation.navigate('Quant_clicks')}
+                        >
+                        <Moldura>
+                            <Text style={{ fontSize: 20 }}>Personagens</Text>
+                        </Moldura>
+                    </Pressable>
+                    <Pressable style={styles.botao}
+                        //onPress={() => navigation.navigate('Quant_clicks')}
+                        >
+                        <Text style={{ fontSize: 20 }}>Monte sua Tripulação</Text>
+                    </Pressable>
+                </View>    
+            </ImageBackground>
         </View>
     );
 }
@@ -23,74 +50,41 @@ export default ({ navigation }: Tela_inicialprops) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        
         backgroundColor: '#008080',
-
-
     },
-
-    footer: {
-        width: 410,
-        height: 200,
-        marginLeft: 3
+    background: {
+        flex: 1,
     },
-    box: {
-        width: 300,
-        height: 40,
-        borderWidth: 1,
-        borderColor: 'black',
-        borderRadius: 5,
-        paddingLeft: 10,
-        backgroundColor: 'white',
-        marginBottom: 20,
-        color: "black",
-        
-
+    header: { 
+        height: 120,
+        alignItems: 'center'
     },
-    botao: {
-        width: 100,
-        height: 100,
-        borderWidth: 1,
-        borderColor: 'black',
-        borderRadius: 5,
-        backgroundColor: '#d4d0cf',
-        marginBottom: 20,
-        alignItems: 'center',
-    
-    },
-    esqueci: {
-        width: 85,
-        height: 45,
-        borderWidth: 1,
-        borderColor: 'black',
-        borderRadius: 5,
-        backgroundColor: '#d4d0cf',
-        //marginBottom: 20,
-        // alignSelf: 'baseline',
-        alignItems: 'center',
-    },
-    conta: {
-        width: 85,
-        height: 45,
-        borderWidth: 1,
-        borderColor: 'black',
-        borderRadius: 5,
-        backgroundColor: '#d4d0cf',
-        //marginBottom: 20,
-        // alignSelf: 'flex-end',
-        alignItems: 'center',
-
-    },
-    botoes: {
-        flexDirection: 'row',
-        alignItems: 'flex-end',
-        justifyContent: 'space-around',
-        flex: 1
-    },
-    center: {
+    body: {
         justifyContent: 'center',
-        alignItems: 'center',
-        flex: 3
+        flex: 1,
+        alignSelf: 'center'
     },
+    logo: {
+        flex: 1,       
+        width: '80%',
+        resizeMode: "center"
+    },
+    
+    botao: {
+        width: 400,
+        height: 300,
+        marginBottom: 20,
+        alignItems: 'center',
+    },
+    moldura: {
+        width: 400,
+        height: 300,
+    },
+    preenchimento_moldura: {
+        width: 350,
+        height: 250,
+        borderRadius: 100,
+        backgroundColor: '#ffffff',
+    }
 }
 );
